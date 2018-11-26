07e24de8d7cf1fc25490a0177698a61a1abba73c
Richard Wilson <richard.wilson@connect-group.com>
2018-11-26 18:38:52 +0000

    Add basic nav bar

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-const NavBar = () => <div>Navbar</div>;
+const styles = {
+  root: {
+    flexGrow: 1
+  }
+};
 
-export default NavBar;
+function NavBar(props) {
+  const { classes } = props;
+
+  return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              GitHubber
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+  );
+}
+
+NavBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(NavBar);
