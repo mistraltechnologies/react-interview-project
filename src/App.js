274258f2a233b470c09797f494919cf9dcbc7994
Richard Wilson <richard.wilson@connect-group.com>
2018-11-26 20:00:04 +0000

    Add search input to nav bar and add styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router} from "react-router-dom";
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import NavBar from './NavBar/NavBar';
 import User from './User/User';
 import Panel from './Panel/Panel';
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core/styles";
+import blue from '@material-ui/core/colors/blueGrey';
+import red from '@material-ui/core/colors/red';
+import pink from '@material-ui/core/colors/pink';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.theme = createMuiTheme({
+      palette: {
+        primary: blue,
+        secondary: pink,
+        error: red,
+        // Used by `getContrastText()` to maximize the contrast between the background and
+        // the text.
+        contrastThreshold: 3,
+        // Used to shift a color's luminance by approximately
+        // two indexes within its tonal palette.
+        // E.g., shift from Red 500 to Red 300 or Red 700.
+        tonalOffset: 0.2,
+      }
+    });
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <CssBaseline/>
-        <Router>
-          <div>
-            <NavBar></NavBar>
-            <User></User>
-            <Panel></Panel>
-            <Panel></Panel>
-          </div>
-        </Router>
-      </React.Fragment>
+        <Fragment>
+          <CssBaseline/>
+          <MuiThemeProvider theme={this.theme}>
+            <Router>
+              <div>
+                <NavBar></NavBar>
+                <User></User>
+                <Panel></Panel>
+                <Panel></Panel>
+              </div>
+            </Router>
+          </MuiThemeProvider>
+        </Fragment>
     );
   }
 }
