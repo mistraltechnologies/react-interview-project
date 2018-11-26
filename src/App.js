9d8cae4d20c1ebef7bb4d0da8c8dd37106ecfa57
Richard Wilson <richard.wilson@connect-group.com>
2018-11-26 18:23:52 +0000

    Add routing and initial top level components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import NavBar from './NavBar/NavBar';
+import User from './User/User';
+import Panel from './Panel/Panel';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <CssBaseline/>
+        <Router>
+          <div>
+            <NavBar></NavBar>
+            <User></User>
+            <Panel></Panel>
+            <Panel></Panel>
+          </div>
+        </Router>
       </React.Fragment>
     );
   }
---
+++ src/Panel/Panel.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Panel = () => <div>Panel</div>;
+
+export default Panel;
