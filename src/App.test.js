76a2c274ac4cbd53b2e1aca9729e6fb0e775f49c
Richard Wilson <richard.wilson@connect-group.com>
2018-11-27 23:16:45 +0000

    Fix existing App component test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {applyMiddleware, createStore} from "redux";
+import rootReducer from "./store/reducer";
+import thunk from "redux-thunk";
 
 it('renders without crashing', () => {
+  const store = createStore(
+      rootReducer,
+      applyMiddleware(thunk));
+
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App store={store}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
